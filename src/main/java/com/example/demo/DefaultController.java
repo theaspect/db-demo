ab49d13c24e613a7254d301fd870082bd34480a8
Constantine Linnick <theaspect@gmail.com>
2018-01-18 00:10:07 +0300

    Add template engine

--- src/main/java/com/example/demo/DefaultController.java
+++ src/main/java/com/example/demo/DefaultController.java
@@ -3,22 +3,33 @@ package com.example.demo;
 import com.example.demo.domain.Employee;
 import com.example.demo.jdbc.JdbcRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
 
-@RestController
-public class Controller {
+@Controller
+public class DefaultController {
 
     @Autowired
     JdbcRequest jdbcRequest;
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello Docker World";
+    @RequestMapping("/page")
+    public String page() {
+        return "page";
+    }
+
+    @RequestMapping("/params")
+    public ModelAndView params(@RequestParam(defaultValue = "0") Long id) {
+        return new ModelAndView("params",
+                new HashMap<String, String>() {{
+                    put("id", id.toString());
+                }});
     }
 
     @RequestMapping(path = "/jdbc", method = RequestMethod.GET)
