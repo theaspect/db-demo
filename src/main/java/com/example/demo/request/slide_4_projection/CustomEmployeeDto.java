bbb477b77313135fb0adee7760e36a90ad78a017
Рябушев С.А <RyabushevSA@alfastrah.ru>
2018-01-27 14:30:47 +0700

    Четвертый слайд. Проекции

---
+++ src/main/java/com/example/demo/request/slide_4_projection/CustomEmployeeDto.java
@@ -0,0 +1,8 @@
+package com.example.demo.request.slide_4_projection;
+
+public class CustomEmployeeDto {
+
+    private Long id;
+    private String firstName;
+
+}
---
+++ src/main/java/com/example/demo/request/slide_4_projection/EmployeeSpringDao.java
@@ -0,0 +1,15 @@
+package com.example.demo.request.slide_4_projection;
+
+import com.example.demo.domain.Department;
+import com.example.demo.domain.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Collection;
+
+public interface EmployeeSpringDao extends CrudRepository<Employee, Long> {
+
+    Collection<Employee> findByDepartment(Department department);
+
+    Collection<CustomEmployeeDto> findCustomEmployeeByDepartment(Department department);
+
+}
