ab14231656821195963154da98862dfd054d29f3
Constantine Linnick <theaspect@gmail.com>
2018-01-27 11:45:31 +0300

    Fix tests

--- src/test/java/com/example/demo/request/slide_5_lazy/EmployeeToDtoConverterTest.java
+++ src/test/java/com/example/demo/request/slide_5_lazy/EmployeeToDtoConverterTest.java
@@ -3,12 +3,16 @@ package com.example.demo.request.slide_5_lazy;
 import com.example.demo.domain.Employee;
 import com.example.demo.request.dao.EmployeeRepository;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
 @SuppressWarnings("ALL")
 @SpringBootTest
 @WebAppConfiguration
@@ -20,7 +24,8 @@ public class EmployeeToDtoConverterTest {
     @Autowired
     EmployeeToDtoConverter converter;
 
-    void testEmployeeConverter() throws Exception {
+    @Test
+    public void testEmployeeConverter() throws Exception {
         Employee employee = repository.findOne(1L);
         EmployeeDto employeeDto = converter.convert(employee);
 
